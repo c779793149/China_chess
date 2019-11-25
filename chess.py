9b739b61f854a87e7db70f78bb1a2469a79b5fe9
Tedu <lvze@tedu.cn>
2019-11-25 08:24:45 +0800

    update

--- chess.py
+++ chess.py
@@ -1,64 +1,301 @@
-class Chess:
-    def __init__(self,name="空",camp="白",position=None):
+class Chess:  # 棋子类
+    """
+    吃子:只能吃敌方棋子,只要符合行棋方式.
+    """
+
+    def __init__(self, name="空", camp="白", position=(None, None)):
+        """
+        象棋棋子类
+        :param name:棋子名
+        :param camp: 棋子阵营
+        :param position: 棋子坐标
+        """
         self.name = name
         self.camp = camp
         self.position = position
-        self.r = self.position[0]
-        self.c = self.position[1]
+        self.choice = []  # 棋子可选移动坐标,该坐标以二维列表索引为基准.
+        # self.nearby = []  # 棋子附近的其他棋子
 
     def run(self):
-        pass
-
-    def eat(self):
-        pass
+        """
+        走棋方式:执红先行,以红棋界面为基准
+        """
+        self.r = self.position[0]
+        self.c = self.position[1]
+        # print(self.r, self.c)
+        self.choice.clear()  # 清空可移动位置列表
+        # self.nearby.clear()  # 清空附近棋子列表
 
     def __str__(self):
         return self.camp + "-" + self.name
 
-class Rook(Chess):
+
+BLANK = Chess()  # 空白棋子对象
+MAP = [[BLANK for i in range(9)] for j in range(10)]  # 用空白棋子初始化棋盘
+
+
+class Rook(Chess):  # 车
+    "车横冲直撞"
+
     def run(self):
-        for r in range(self.r-1,-1):
-            pass
-        for r in range(self.r+1,9)
-            pass
-        for c in range()
+        super(Rook, self).run()
+        self.transverse_move(self.c + 1, 9)  # 筛选右侧可移动位置
+        self.transverse_move(self.c - 1, -1, -1)  # 筛选左侧可移动位置
+        self.Vertical_move(self.r + 1, 10)  # 筛选下侧可移动位置
+        self.Vertical_move(self.r - 1, -1, -1)  # 筛选上侧可移动位置
 
-    def eat(self):
-        pass
+    # 横移坐标筛选
+    def transverse_move(self, begin, end, sep=1):
+        for c in range(begin, end, sep):
+            if MAP[self.r][c] != BLANK:  # 如果检测到有棋子
+                # print("附近:", MAP[self.r][c], ":", MAP[self.r][c].position)
+                if MAP[self.r][c].camp != self.camp:  # 如果棋子不是一个阵营
+                    self.choice.append((self.r, c))  # 加入可移动位置
+                break
+            self.choice.append((self.r, c))
 
-class Knight(Chess):
-    pass
-class Elephant(Chess):
-    pass
-
-class Mandarin(Chess):
-    pass
-
-class King(Chess):
-    pass
-
-class Cannon(Chess):
-    pass
-
-class Pawn(Chess):
-    pass
+    # 竖移坐标筛选
+    def Vertical_move(self, begin, end, sep=1):
+        for r in range(begin, end, sep):
+            if MAP[r][self.c] != BLANK:  # 如果检测到有棋子
+                # print("附近:", MAP[r][self.c], ":", MAP[r][self.c].position)
+                if MAP[r][self.c].camp != self.camp:  # 如果棋子不是一个阵营
+                    self.choice.append((r, self.c))
+                break
+            self.choice.append((r, self.c))
 
 
-# def main():
-#     map = [
-#     ["车","马","相","士","帅","士","相","马","车"],
-#     ["","","","","","","","","","","","",],
-#     ["","炮","","","","","","炮",""],
-#     ["卒","","卒","","卒","","卒","","卒"],
-#     ["","","","","","","","",""],
-#     ["","","","","","","","",""],
-#     ["卒","","卒","","卒","","卒","","卒"],
-#     ["","炮","","","","","","炮",""],
-#     ["","","","","","","","",""],
-#     ["车","马","象","士","将","士","象","马","车"]
-# ]
-#
-#     for line in map:
-#         for item in line:
-#             print(item,end="\t")
-#         print()
+class Knight(Chess):  # 马
+    """
+    马走"日"对角,当"日"字中间同侧有棋时,无法移动
+    """
+
+    def run(self):
+        super(Knight, self).run()
+        self.up_choice()
+        self.down_choice()
+        self.left_choice()
+        self.right_choice()
+
+    # 筛选上移位置
+    def up_choice(self):
+        if self.r - 2 < 0: return
+        self.Vertical_move(self.r - 1, self.r - 2)
+
+    # 筛选下移位置
+    def down_choice(self):
+        if self.r + 2 > 9: return
+        self.Vertical_move(self.r + 1, self.r + 2)
+
+    # 竖移坐标筛选
+    def Vertical_move(self, a, b):
+        if MAP[a][self.c] != BLANK: return
+        if self.c - 1 > -1 and MAP[b][self.c - 1].camp != self.camp:
+            self.choice.append((b, self.c - 1))
+        if self.c + 1 < 9 and MAP[b][self.c + 1].camp != self.camp:
+            self.choice.append((b, self.c + 1))
+
+    # 筛选左移位置
+    def left_choice(self):
+        if self.c - 2 < 0: return
+        self.transverse_move(self.c - 1, self.c - 2)
+
+    # 筛选右移位置
+    def right_choice(self):
+        if self.c + 2 > 8: return
+        self.transverse_move(self.c + 1, self.c + 2)
+
+    # 横移坐标筛选
+    def transverse_move(self, a, b):
+        if MAP[self.r][a] != BLANK: return
+        if self.r - 1 > 0 and MAP[self.r - 1][b].camp != self.camp:
+            self.choice.append((self.r - 1, b))
+        if self.r + 1 < 10 and MAP[self.r + 1][b].camp != self.camp:
+            self.choice.append((self.r + 1, b))
+
+
+class Elephant(Chess):  # 象/像
+    """
+    象走"田"对角,不能过河,当"田"字中间有棋时不能移动
+    """
+
+    def run(self):
+        super(Elephant, self).run()
+        self.assign_region()  # 需区分阵营,划定行棋区域(相/象不能过河)
+        self.do_choice(-1, -1)  # 确认左上角是否可移动
+        self.do_choice(1, -1)  # 确认右上角是否可移动
+        self.do_choice(-1, 1)  # 确认左下角是否可移动
+        self.do_choice(1, 1)  # 确认右下角是否可移动
+
+    def assign_region(self):
+        """
+        分阵营初始化可移动行边界
+        """
+        if self.camp == "红":
+            self.row_max = 9
+            self.row_min = 5
+        else:
+            self.row_max = 4
+            self.row_min = 0
+
+    # 确认是否可移动
+    def do_choice(self, abscissa, ordinate):
+        """
+        综合判断象/象是否可以移动
+        :param abscissa: 横坐标,1表示右移,-1表示左移
+        :param ordinate: 纵坐标,1表示下移,-1表示上移
+        """
+        if self.r + ordinate * 2 < self.row_min and ordinate == -1: return  # 判断上移的行
+        if self.r + ordinate * 2 > self.row_max and ordinate == 1: return  # 判断下移的行
+        if self.c + abscissa * 2 < 0 and abscissa == -1: return  # 判断左移的列
+        if self.c + abscissa * 2 > 8 and abscissa == 1: return  # 判断右移的列
+        if MAP[self.r + ordinate][self.c + abscissa] == BLANK:
+            if MAP[self.r + ordinate * 2][self.c + abscissa * 2].camp != self.camp:
+                self.choice.append((self.r + ordinate * 2, self.c + abscissa * 2))
+
+
+class Mandarin(Chess):  # 士
+    """
+    士只能在九宫格的四角及中间以"口"字对角行棋
+    """
+
+    def run(self):
+        super(Mandarin, self).run()
+        self.assign_region()
+        self.do_choice()
+
+    def assign_region(self):
+        """
+        分阵营初步指定可选坐标
+        """
+        if self.camp == "红":
+            self.choice[:] = [(8, 4), (9, 3), (9, 5), (7, 3), (7, 5)]
+        else:
+            self.choice[:] = [(1, 4), (0, 3), (0, 5), (2, 3), (2, 5)]
+
+    def do_choice(self):
+        # 如果当前位置不在九宫中心,那么可移动位置只能是九宫中心
+        # 反之,除了同阵营占有,都可以移动
+        if self.position != self.choice[0]:
+            self.choice[:] = self.choice[0:1]
+        for i in range(len(self.choice) - 1, -1, -1):
+            if MAP[self.choice[i][0]][self.choice[i][1]].camp == self.camp:
+                del self.choice[i]  # 如果阵营相同则不能移动
+
+
+class Cannon(Chess):  # 炮
+    """
+    炮横冲直撞,隔山打牛.
+    """
+
+    def run(self):
+        super(Cannon, self).run()
+        self.transverse_move(self.c + 1, 9)  # 筛选右侧可移动位置
+        self.transverse_move(self.c - 1, -1, -1)  # 筛选左侧可移动位置
+        self.Vertical_move(self.r + 1, 10)  # 筛选下侧可移动位置
+        self.Vertical_move(self.r - 1, -1, -1)  # 筛选上侧可移动位置
+
+    # 左移或右移坐标筛选
+    def transverse_move(self, begin, end, sep=1):
+        for c in range(begin, end, sep):
+            if MAP[self.r][c] != BLANK:  # 如果检测到有棋子
+                for cc in range(c + sep, end, sep):
+                    if MAP[self.r][cc] != BLANK:
+                        if MAP[self.r][c].camp != self.camp:  # 如果棋子不是一个阵营
+                            self.choice.append((self.r, cc))  # 加入可移动位置
+                        break
+                break
+            self.choice.append((self.r, c))
+
+    def Vertical_move(self, begin, end, sep=1):
+        for r in range(begin, end, sep):
+            if MAP[r][self.c] != BLANK:  # 如果检测到有棋子
+                for rr in range(r + sep, end, sep):
+                    if MAP[rr][self.c] != BLANK:
+                        if MAP[rr][self.c].camp != self.camp:  # 如果棋子不是一个阵营
+                            self.choice.append((rr, self.c))  # 加入可移动位置
+                        break
+                break
+            self.choice.append((r, self.c))
+
+
+class Pawn(Chess):  # 兵卒
+    """
+    兵卒死战不退,不入敌阵不横移
+    """
+
+    def run(self):
+        super(Pawn, self).run()
+        if self.camp == "红":
+            self.red_choice()
+        else:
+            self.black_choice()
+
+    # 红棋移动坐标筛选
+    def red_choice(self):
+        if self.r > 0:
+            if MAP[self.r - 1][self.c].camp != self.camp:
+                self.choice.append((self.r - 1, self.c))
+        if self.r < 5:
+            self.transverse_move()
+
+    # 黑棋移动坐标筛选
+    def black_choice(self):
+        if self.r < 9:
+            if MAP[self.r + 1][self.c].camp != self.camp:
+                self.choice.append((self.r + 1, self.c))
+        if self.r > 4:
+            self.transverse_move()
+
+    # 平移坐标筛选
+    def transverse_move(self):
+        if self.c > 0:
+            if MAP[self.r][self.c - 1].camp != self.camp:
+                self.choice.append((self.r, self.c - 1))
+        if self.c < 8:
+            if MAP[self.r][self.c + 1].camp != self.camp:
+                self.choice.append((self.r, self.c + 1))
+
+
+class King(Chess):  # 将/帅
+    """
+    输赢待定,先确认走棋方式
+    将帅只能在九宫格内走直线,一次只能移动一格
+    """
+
+    def run(self):
+        super(King, self).run()
+        self.assign_region()
+        self.do_choice()
+
+    def assign_region(self):
+        """
+        分阵营初步指定可选坐标
+        """
+        if self.camp == "红":
+            self.choice[:] = [
+                (9, 3), (9, 4), (9, 5),
+                (8, 3), (8, 4), (8, 5),
+                (7, 3), (7, 4), (7, 5)
+            ]
+        else:
+            self.choice[:] = [
+                (0, 3), (0, 4), (0, 5),
+                (1, 3), (1, 4), (1, 5),
+                (2, 3), (2, 4), (2, 5)
+            ]
+
+    def do_choice(self):
+        """
+        删除九宫格内同意阵营棋子
+        删除不在一条直线上的棋子
+        删除距离二格的棋子
+        """
+        for i in range(len(self.choice) - 1, -1, -1):
+            if MAP[self.choice[i][0]][self.choice[i][1]].camp == self.camp:
+                del self.choice[i]
+            elif self.choice[i][0] != self.r and self.choice[i][1] != self.c:
+                del self.choice[i]
+            elif self.choice[i][0] - self.r not in (1, -1) and \
+                    self.choice[i][1] - self.c not in (1, -1):
+                del self.choice[i]
--- master.py
+++ master.py
@@ -1,17 +1,19 @@
+import os
+
 from chess import *
 
 
 class Manager:
     def __init__(self):
         self.create_chess()
-        self.map = [[self.blank for i in range(9)] for j in range(10)]
+        # self.map = [[self.blank for i in range(9)] for j in range(10)]
         self.chess = [
             self.rook1_red,
             self.knight1_red,
             self.ele1phant1_red,
             self.mandarin1_red,
-            self.king,
-            self.ele1phant2_red,
+            self.general,
+            self.mandarin2_red,
             self.ele1phant2_red,
             self.knight2_red,
             self.rook2_red,
@@ -33,105 +35,105 @@ class Manager:
             self.knight1_black,
             self.ele1phant1_black,
             self.mandarin1_black,
-            self.general,
+            self.king,
             self.mandarin2_black,
             self.ele1phant2_black,
             self.knight2_black,
             self.rook2_black
         ]
+        self.chess.reverse()
 
     def create_chess(self):
-        self.rook1_red = Rook("车", "红",(9,0))
-        self.rook2_red = Rook("车", "红",(9,8))
-        self.rook1_black = Rook("车", "黑",(0,0))
-        self.rook2_black = Rook("车", "黑",(0,8))
-        self.knight1_red = Knight("马", "红",(9,1))
-        self.knight2_red = Knight("马", "红",(9,7))
-        self.knight1_black = Knight("马", "黑",(0,1))
-        self.knight2_black = Knight("马", "黑",(0,7))
-        self.ele1phant1_red = Elephant("相", "红",(9,2))
-        self.ele1phant2_red = Elephant("相", "红",(9,6))
-        self.ele1phant1_black = Elephant("象", "黑",(0,2))
-        self.ele1phant2_black = Elephant("象", "黑",(0,6))
-        self.mandarin1_red = Mandarin("士", "红",(9,3))
-        self.mandarin2_red = Mandarin("士", "红",(9,5))
-        self.mandarin1_black = Mandarin("士", "黑",(0,3))
-        self.mandarin2_black = Mandarin("士", "黑",(0,5))
-        self.general = King("帅", "红",(9,4))
-        self.king = King("帅", "黑",(0,4))
-        self.cannon1_red = Cannon("炮", "红",(7,1))
-        self.cannon2_red = Cannon("炮", "红",(7,7))
-        self.cannon1_black = Cannon("炮", "黑",(2,1))
-        self.cannon2_black = Cannon("炮", "黑",(2,7))
-        self.pawn1_red = Pawn("兵", "红",(6,0))
-        self.pawn2_red = Pawn("兵", "红",(6,2))
-        self.pawn3_red = Pawn("兵", "红",(6,4))
-        self.pawn4_red = Pawn("兵", "红",(6,6))
-        self.pawn5_red = Pawn("兵", "红",(6,8))
-        self.pawn1_black = Pawn("卒", "黑",(3,0))
-        self.pawn2_black = Pawn("卒", "黑",(3,2))
-        self.pawn3_black = Pawn("卒", "黑",(3,4))
-        self.pawn4_black = Pawn("卒", "黑",(3,6))
-        self.pawn5_black = Pawn("卒", "黑",(3,8))
-        self.blank = Chess()
+        self.rook1_red = Rook("车", "红", (9, 0))
+        self.rook2_red = Rook("车", "红", (9, 8))
+        self.rook1_black = Rook("车", "黑", (0, 0))
+        self.rook2_black = Rook("车", "黑", (0, 8))
+        self.knight1_red = Knight("马", "红", (9, 1))
+        self.knight2_red = Knight("马", "红", (9, 7))
+        self.knight1_black = Knight("马", "黑", (0, 1))
+        self.knight2_black = Knight("马", "黑", (0, 7))
+        self.ele1phant1_red = Elephant("相", "红", (9, 2))
+        self.ele1phant2_red = Elephant("相", "红", (9, 6))
+        self.ele1phant1_black = Elephant("象", "黑", (0, 2))
+        self.ele1phant2_black = Elephant("象", "黑", (0, 6))
+        self.mandarin1_red = Mandarin("士", "红", (9, 3))
+        self.mandarin2_red = Mandarin("士", "红", (9, 5))
+        self.mandarin1_black = Mandarin("士", "黑", (0, 3))
+        self.mandarin2_black = Mandarin("士", "黑", (0, 5))
+        self.general = King("帅", "红", (9, 4))
+        self.king = King("将", "黑", (0, 4))
+        self.cannon1_red = Cannon("炮", "红", (7, 1))
+        self.cannon2_red = Cannon("炮", "红", (7, 7))
+        self.cannon1_black = Cannon("炮", "黑", (2, 1))
+        self.cannon2_black = Cannon("炮", "黑", (2, 7))
+        self.pawn1_red = Pawn("兵", "红", (6, 0))
+        self.pawn2_red = Pawn("兵", "红", (6, 2))
+        self.pawn3_red = Pawn("兵", "红", (6, 4))
+        self.pawn4_red = Pawn("兵", "红", (6, 6))
+        self.pawn5_red = Pawn("兵", "红", (6, 8))
+        self.pawn1_black = Pawn("卒", "黑", (3, 0))
+        self.pawn2_black = Pawn("卒", "黑", (3, 2))
+        self.pawn3_black = Pawn("卒", "黑", (3, 4))
+        self.pawn4_black = Pawn("卒", "黑", (3, 6))
+        self.pawn5_black = Pawn("卒", "黑", (3, 8))
+        # self.blank = Chess()
 
-    def output_map_black(self):
+    def output_map(self, camp):
         for chess in self.chess:
+            # print(chess,":",chess.position)
             r = chess.position[0]
             c = chess.position[1]
-            self.map[r][c]=chess
-        self.map_Matrix_transposition()
-        for row in self.map:
+            MAP[r][c] = chess
+        if camp == "B": self.map_Matrix_transposition()
+        for row in MAP:
             for chess in row:
-                print(chess,end=" ")
+                print(chess, end=" ")
             print()
-        self.map_Matrix_transposition()
-
-    def output_map_red(self):
-        for chess in self.chess:
-            r = chess.position[0]
-            c = chess.position[1]
-            self.map[r][c]=chess
-        for row in self.map:
-            for chess in row:
-                print(chess,end=" ")
-            print()
+        if camp == "B": self.map_Matrix_transposition()
 
     def start(self):
         while True:
-            p = input("请输入阵营(r/b):").upper()
-            if p == "R":
-                self.output_map_red()
-            elif p.upper() == "B":
-                self.output_map_black()
-            else:
-                continue
+            # p = input("请输入阵营(r/b):").upper()
+            # if p not in ("B","R"):
+            #     continue
+            # else:
+            p = "r"
+            self.output_map(p)
             data = input("请输入操作(输入坐标即可):")
-            p0,p1=data.split(" ")
+            os.system("cls")
+            # if not data:
+            #     continue
+            # else:
+            #     p0, p1 = data.split(" ")
+            #     p0 = self.str_to_tuple(p0)
+            #     p1 = self.str_to_tuple(p1)
+            # if p == "B":
+            #     p0 = (9 - p0[0], 9 - p0[1])
+            #     p1 = (9 - p1[0], 9 - p1[1])
+            #     print(p0, p1)
+
+            p0, p1 = data.split(" ")
             p0 = self.str_to_tuple(p0)
             p1 = self.str_to_tuple(p1)
-            if not data:
-                continue
+            MAP[p0[0]][p0[1]].run()
+            print("当前棋子",MAP[p0[0]][p0[1]],MAP[p0[0]][p0[1]].position)
+            print("可移动位置",MAP[p0[0]][p0[1]].choice)
+            if p1 in MAP[p0[0]][p0[1]].choice:
+                if MAP[p1[0]][p1[1]] != BLANK :
+                    self.chess.remove(MAP[p1[0]][p1[1]])
+                    print(len(self.chess))
+                    print("吃子:",MAP[p1[0]][p1[1]])
+                MAP[p0[0]][p0[1]].position = p1
+                MAP[p0[0]][p0[1]] = BLANK
             else:
-                if p == "B":
-                    p0 = (8-p0[0],8-p0[1])
-                    p1 = (8-p1[0],8-p1[1])
-                    print(p0,p1)
-                self.map[p0[0]][p0[1]].position = p1
-                self.map[p0[0]][p0[1]] = self.blank
+                print(p1,"非法操作!")
 
-    #将输入的字符串形式元组转化为元组
-    def str_to_tuple(self,data):
+    # 将输入的字符串形式元组转化为元组
+    def str_to_tuple(self, data):
         try:
-            return tuple(map(int,data[1:-1].split(",")))
+            return tuple(map(int, data[1:-1].split(",")))
         except:
             return ()
-
-    def map_Matrix_transposition(self):
-        for line in self.map:
-            line.reverse()
-        self.map.reverse()
-
 
 if __name__ == "__main__":
     manager = Manager()
